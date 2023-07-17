7796ce7582ccce98a4536b5d81edc170719f19d9
Henrique de Freitas Almeida <56054329+detowhey@users.noreply.github.com>
2023-07-17 00:45:23 -0300

    Refactor JsonProperty of UserProfileResponse

--- src/main/kotlin/br/dev/henriquealmeida/usergithub/dto/response/UserProfileResponse.kt
+++ src/main/kotlin/br/dev/henriquealmeida/usergithub/dto/response/UserProfileResponse.kt
@@ -1,5 +1,6 @@
 package br.dev.henriquealmeida.usergithub.dto.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(name = "UserProfile", description = "User data")
@@ -15,5 +16,6 @@ data class UserProfileResponse(
     @field:Schema(description = "GitHub user create date profile", pattern = "dd/MM/yyyy HH:mm", example = "10/02/2020 14:21")
     val createDate: String,
     @field:Schema(description = "GitHub user number of public repositories ", example = "5", type = "Number")
+    @field:JsonProperty("amountRepositories")
     val numberPublicRepos: Long
 )
